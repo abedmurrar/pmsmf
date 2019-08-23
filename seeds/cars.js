300449cdf39bb67860adf20355d1f6e54a679895
abedmurrar <abedmurrar15@gmail.com>
2019-08-24 02:26:00 +0300

    modify car seeds for required fields

--- seeds/cars.js
+++ seeds/cars.js
@@ -5,9 +5,15 @@ exports.seed = function(knex) {
         .then(function() {
             // Inserts seed entries
             return knex('cars').insert([
-                { id: 1, manufacturer: 'SEAT', model: 'IBIZA' },
-                { id: 2, manufacturer: 'BMW', model: 'E30' },
-                { id: 3, manufacturer: 'MERCEDES', model: 'C180' }
+                { id: 1, manufacturer: 'SEAT', model: 'IBIZA', car_class: 'A', push_type: 'Front' },
+                { id: 2, manufacturer: 'BMW', model: 'E30', car_class: 'A', push_type: 'REAR' },
+                {
+                    id: 3,
+                    manufacturer: 'MERCEDES',
+                    model: 'C180',
+                    car_class: 'A',
+                    push_type: 'REAR'
+                }
             ]);
         });
 };
