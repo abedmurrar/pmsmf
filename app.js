b937362cd4f35aa83b36002f9e14e7d2bf6783bf
abedmurrar <abedmurrar15@gmail.com>
2019-08-27 19:03:33 +0300

    add ejs view engine

--- app.js
+++ app.js
@@ -2,12 +2,14 @@
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const morgan = require('morgan');
+const fs = require('fs');
 
 /* Routers */
 const { Model } = require('objection');
-const carsRouter = require('./routes/cars');
-const usersRouter = require('./routes/users');
+const carsRouter = require('./routes/api/cars');
+const usersRouter = require('./routes/api/users');
+const indexRouter = require('./routes/index');
 
 /* Database configuration */
 const knex = require('./db/config');
@@ -17,15 +19,36 @@ Model.knex(knex);
 /* Application start */
 const app = express();
 
+/* View Engine EJS */
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 /* Middlewares */
-app.use(logger('dev'));
+morgan.token('body', function(req, res) {
+    return JSON.stringify(req.body);
+});
+morgan.token('headers', function(req, res) {
+    // TODO: update the behavior
+    let string = '';
+    console.log(string);
+    return string;
+});
+app.use(morgan('dev'));
+app.use(
+    morgan('":method :url :http-version\n:headers\n\n :body"\n:status :response-time ms\n\n', {
+        stream: fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
+    })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+/* Serve static files from public folder */
 app.use(express.static(path.join(__dirname, 'public')));
 
 /* Routers */
-app.use('/cars', carsRouter);
-app.use('/users', usersRouter);
+app.use('/', indexRouter);
+
+app.use('/api', [carsRouter, usersRouter]);
 
 module.exports = app;
