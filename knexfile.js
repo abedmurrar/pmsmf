e0d5abd8ed9ccc642ac9ad060b1e3cf1f7862e24
abedmurrar <abedmurrar15@gmail.com>
2019-08-27 19:04:32 +0300

    update knex development config

--- knexfile.js
+++ knexfile.js
@@ -1,23 +1,48 @@
-// Update with your config settings.
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-console */
+const path = require('path');
+const debug = require('debug')('knex_config');
 
 module.exports = {
     development: {
         client: 'mysql',
+        debug: true,
         connection: {
             host: '127.0.0.1',
             user: 'admin',
             password: '246805@Abed',
             database: 'pmsmf'
         },
+        log: {
+            warn(message) {
+                debug(message);
+                console.warn(message);
+            },
+            error(message) {
+                debug(message);
+                console.error(message);
+            },
+            deprecate(message) {
+                debug(message);
+                console.info(message);
+            },
+            debug(message) {
+                debug(message);
+                console.debug(message);
+            }
+        },
         migrations: {
+            directory: path.join(__dirname, '/db/migrations/'),
             tableName: 'knex_migrations'
+        },
+        seeds: {
+            directory: path.join(__dirname, '/db/seeds/dev')
         }
     },
-
     production: {
         client: 'postgresql',
         connection: {
-            database: 'my_db',
+            database: 'pmsmf',
             user: 'username',
             password: 'password'
         },
@@ -26,7 +51,11 @@ module.exports = {
             max: 10
         },
         migrations: {
-            tableName: 'knex_migrations'
+            directory: path.join(__dirname, '/db/migrations/'),
+            tableName: 'migrations'
+        },
+        seeds: {
+            directory: path.join(__dirname, '/db/seeds/prod')
         }
     }
 };
