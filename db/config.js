143633697ad4301d356dd7b1b83d612390527dc1
abedmurrar <abedmurrar15@gmail.com>
2019-08-31 23:10:00 +0300

    migrate and seed on start

--- db/config.js
+++ db/config.js
@@ -1,8 +1,22 @@
+/* eslint-disable no-console */
 const knex = require('knex');
 const knexfile = require('../knexfile');
+const debug = require('debug')('knexconfig:log');
 
 const env = process.env.NODE_ENV === 'production' ? 'production' : 'development';
 
 const knexConfig = knex(knexfile[env]);
+
+knexConfig.migrate
+    .latest()
+    .then(() => {
+        return knexConfig.seed.run();
+    })
+    .then(() => {
+        debug('Migrations finished')
+    })
+    .catch(err => {
+        console.error(err);
+    });
 
 module.exports = knexConfig;
