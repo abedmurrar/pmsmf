8b2f00761641b4db33d26d78e28714d264231e31
abedmurrar <abedmurrar15@gmail.com>
2019-08-31 23:10:57 +0300

    update settings

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 /* Main Dependencies */
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const morgan = require('morgan');
 const fs = require('fs');
 
-/* Routers */
+/* Session & Database configuration */
 const { Model } = require('objection');
-const carsRouter = require('./routes/api/cars');
-const usersRouter = require('./routes/api/users');
-const indexRouter = require('./routes/index');
-
-/* Database configuration */
+const KnexSessionStore = require('connect-session-knex')(session);
 const knex = require('./db/config');
 
 Model.knex(knex);
+
+/* Get all routers */
+const carsRouter = require('./routes/api/cars');
+const driversRouter = require('./routes/api/drivers');
+const indexRouter = require('./routes/index');
 
 /* Application start */
 const app = express();
@@ -41,7 +42,23 @@ app.use(
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+
+// TODO: Use helmet
+
+/* Session Settings */
+app.set('trust proxy', 1); // trust first proxy
+app.use(
+    session({
+        store: new KnexSessionStore({ knex, tablename: 'sessions' }),
+        resave: true,
+        saveUninitialized: true,
+        secret: process.env.SESSION_SECRET,
+        cookie: {
+            signed: true,
+            maxAge: 30000 // 30 seconds for testing
+        }
+    })
+);
 
 /* Serve static files from public folder */
 app.use(express.static(path.join(__dirname, 'public')));
@@ -49,6 +66,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 /* Routers */
 app.use('/', indexRouter);
 
-app.use('/api', [carsRouter, usersRouter]);
+app.use('/api', [carsRouter, driversRouter]);
 
 module.exports = app;
