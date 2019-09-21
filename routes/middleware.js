00f8d752f5d78a5cfe2cc4e0310c96f96e53f4dd
Abed Murrar <abed.murrar@mada.ps>
2019-09-21 11:07:36 +0300

    add body validators for all models

--- routes/middleware.js
+++ routes/middleware.js
@@ -1,12 +1,29 @@
 const { body, sanitizeBody } = require('express-validator');
 
+const timeRegex = /^[0-9]{1,2}:[0-9]{1,2}:[0-9]{1,2}:[0-9]{1,2}$/;
+
 const noQueryParams = (req, res, next) => {
-    console.log(!!req.params.id);
     if (!req.params.id) {
-        console.log('no id');
         next(new Error('not found'));
     }
 };
+
+const userValidation = [
+    body('first_name')
+        .isString()
+        .trim()
+        .escape(),
+    body('last_name')
+        .isString()
+        .trim()
+        .escape(),
+    body('username')
+        .isString()
+        .isLength({ max: 20 })
+        .trim()
+        .escape(),
+    body('password').isString()
+];
 
 const carValidation = [
     body('manufacturer')
@@ -19,6 +36,10 @@ const carValidation = [
         .escape(),
     body('year_of_production').matches('^[0-9]{4}$'),
     body('license_no').matches('^[A-Za-z0-9]{6,7}$'),
+    body('motor_capacity')
+        .isString()
+        .trim()
+        .escape(),
     body('car_class')
         .isString()
         .trim()
@@ -26,7 +47,106 @@ const carValidation = [
     body('push_type')
         .isString()
         .trim()
+        .escape(),
+    body('remarks')
+        .isString()
+        .trim()
         .escape()
 ];
 
-module.exports = { noQueryParams, carValidation };
+const driverValidation = [
+    body('first_name')
+        .isString()
+        .trim()
+        .escape(),
+    body('last_name')
+        .isString()
+        .trim()
+        .escape(),
+    body('nationality')
+        .isString()
+        .trim()
+        .escape(),
+    body('address')
+        .isString()
+        .trim()
+        .escape(),
+    body('id_card_no')
+        .isString()
+        .isNumeric() // TODO: Check if this is the right way
+        .trim()
+        .escape(),
+    body('mobile')
+        .isString()
+        .isNumeric()
+        .trim()
+        .escape(),
+    body('email')
+        .isEmail()
+        .normalizeEmail(),
+    body('sponsor')
+        .isString()
+        .trim()
+        .escape(),
+    body('car_id').isInt(),
+    body('remarks')
+        .isString()
+        .trim()
+        .escape()
+];
+
+const rallyValidation = [
+    body('name')
+        .isString()
+        .trim()
+        .escape(),
+    body('city')
+        .isString()
+        .trim()
+        .escape(),
+    body('date')
+        .isString()
+        .toDate()
+        .trim()
+        .escape(),
+    body('type').isInt(),
+    body('remarks')
+        .isString()
+        .trim()
+        .escape()
+];
+
+const speedValidation = [
+    body('rally_id').isInt(),
+    body('driver_id').isInt(),
+    body('racing_number').isInt(),
+    body('time_1').matches(timeRegex),
+    body('time_2').matches(timeRegex),
+    body('time_3').matches(timeRegex),
+    body('best_time').matches(timeRegex),
+    body('fouls_1').isInt(),
+    body('fouls_2').isInt(),
+    body('fouls_3').isInt()
+];
+
+const driftValidation = [
+    body('rally_id').isInt(),
+    body('driver_id').isInt(),
+    body('racing_number').isInt(),
+    body('score_3').isInt(),
+    body('score_1').isInt(),
+    body('score_2').isInt(),
+    body('fouls_1').isInt(),
+    body('fouls_2').isInt(),
+    body('fouls_3').isInt()
+];
+
+module.exports = {
+    noQueryParams,
+    userValidation,
+    carValidation,
+    driverValidation,
+    rallyValidation,
+    speedValidation,
+    driftValidation
+};
