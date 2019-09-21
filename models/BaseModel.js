403bd0f59e47ba7dd2bbd88b4b574d48760ad746
abedmurrar <abedmurrar15@gmail.com>
2019-09-21 07:47:16 +0300

    create custom query for jquery datatables

--- models/BaseModel.js
+++ models/BaseModel.js
@@ -1,5 +1,36 @@
-const { Model } = require('objection');
+/* eslint-disable max-classes-per-file */
+const { Model, QueryBuilder } = require('objection');
 const { DBErrors } = require('objection-db-errors');
+
+class DataTableQueryBuilder extends QueryBuilder {
+
+    selectWithPagination(options) {
+        const { columns = [], order = [], start, length, search, _ } = options;
+        let columnsNames = columns.map(column => column.data);
+        const eagerColumnNames = columnsNames.filter(column => {
+            const isEager = column in this.modelClass().relationMappings;
+            if (isEager) {
+                columnsNames = columnsNames.filter(e => e !== column);
+            }
+            return isEager;
+        });
+
+        const offset = parseInt(start, 10);
+        const eager = `[${eagerColumnNames.join(',')}]`;
+
+        const orders = order.map(orderObj => {
+            return { column: columnsNames[orderObj.column], order: orderObj.dir };
+        });
+        return this.select(columnsNames)
+            .skipUndefined()
+            .eager(eager)
+            .offset(offset)
+            .limit(length)
+            .range()
+            .orderBy(orders)
+            .throwIfNotFound();
+    }
+}
 
 class BaseModel extends DBErrors(Model) {
     static get idColumn() {
@@ -12,6 +43,10 @@ class BaseModel extends DBErrors(Model) {
 
     static get useLimitInFirst() {
         return true;
+    }
+
+    static get QueryBuilder() {
+        return DataTableQueryBuilder;
     }
 }
 
