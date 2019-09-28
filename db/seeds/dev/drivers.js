9a40c06413d609de74c253525651d9f976323f5f
abedmurrar <abedmurrar15@gmail.com>
2019-09-28 03:29:53 +0300

    update driver seeds

--- db/seeds/dev/drivers.js
+++ db/seeds/dev/drivers.js
@@ -1,7 +1,22 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const faker = require('faker');
+const { Car } = require('../../../models');
 
-const createFakeDriver = () => ({
+const getMinMaxCarID = async () => {
+    const values = await Car.query()
+        .select([
+            Car.query()
+                .min('id')
+                .as('min'),
+            Car.query()
+                .max('id')
+                .as('max')
+        ])
+        .first();
+    return values;
+};
+
+const createFakeDriver = (min, max) => ({
     first_name: faker.name.firstName(),
     last_name: faker.name.lastName(),
     nationality: faker.address.country(),
@@ -10,13 +25,22 @@ const createFakeDriver = () => ({
     mobile: faker.phone.phoneNumber('059#######'),
     email: faker.internet.email(),
     sponsor: faker.company.companyName(),
-    car_id: faker.random.number({ min: 1, max: 100 })
+    car_id: faker.random.number({
+        min: min,
+        max: max
+    })
 });
 exports.seed = function(knex) {
-    // const fakeDrivers = [];
-    // const desiredFakeDrivers = 100;
-    // for (let i = 0; i < desiredFakeDrivers; i++) {
-    //     fakeDrivers.push(createFakeDriver());
-    // }
-    // return knex('drivers').insert(fakeDrivers);
+    // return getMinMaxCarID()
+    //     .then(({ min, max }) => {
+    //         const fakeDrivers = [];
+    //         const desiredFakeDrivers = 150;
+    //         for (let i = 0; i < desiredFakeDrivers; i++) {
+    //             fakeDrivers.push(createFakeDriver(min, max));
+    //         }
+    //         return fakeDrivers;
+    //     })
+    //     .then(drivers => {
+    //         return knex('drivers').insert(drivers);
+    //     });
 };
